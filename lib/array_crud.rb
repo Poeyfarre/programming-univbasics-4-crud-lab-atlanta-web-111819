a6fd2d9c48561c62471e9b63fc10bbacf3cf43a1
Jacy Leech <jacy.atlanta@gmail.com>
2019-11-10 04:52:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  music = ["john", "paul", "george", "ringo"]
-  music.push("mick")
+  array = ["john", "paul", "george", "ringo"]
+  array.push("mick")
 end
 
 def add_element_to_start_of_array(array, element)
